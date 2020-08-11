fec1db5ec933ae908164309819cadbf147bb0401
Juan Manuel <juanma.otegui@gmail.com>
2020-08-10 23:16:58 -0300

    Updates

---
+++ precache-manifest.b8fbedf95cf22d928420dd65633908f2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "102c0c90bef5c1618a60242d5ffee0e6",
+    "url": "/mapa-padrones/index.html"
+  },
+  {
+    "revision": "84fe7a61dcaf80f900f4",
+    "url": "/mapa-padrones/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1085cfb3240cf9b3f677",
+    "url": "/mapa-padrones/static/js/2.84a63c81.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mapa-padrones/static/js/2.84a63c81.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84fe7a61dcaf80f900f4",
+    "url": "/mapa-padrones/static/js/main.50aecbee.chunk.js"
+  },
+  {
+    "revision": "8a731f38a63002df21ab",
+    "url": "/mapa-padrones/static/js/runtime-main.c934553b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mapa-padrones/static/media/logo.5d5d9eef.svg"
+  }
+]);
